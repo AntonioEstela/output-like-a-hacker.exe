d74638b4c24768118d98feccc53de5daa1501998
Antonio Estela <antonioestela73@gmail.com>
2021-05-23 12:08:11 -0500

    It works, i just need to add more fake data to print

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 #!/usr/bin/python3
 import os
+from shutil import rmtree
 from sys import stdout
 import time
 
-string = "ANTONIO"
 
-if os.path.exists('files/'):
-    """making a new directory to put all the tmp files"""
-    os.mkdir('files')
+def printWithDelay(string):
+    """Prints a string with a small amounnt of delay between each character"""
+    for let in string:
+        """"""
+        stdout.write(let)
+        stdout.flush()
+        time.sleep(0.001)
+    print()
 
-"""Prints a string with a small amounnt of delay between each character"""
-for let in string:
-    """"""
-    stdout.write(let)
-    stdout.flush()
-    time.sleep(0.01)
-print()
+def openFile(path):
+    """Function that opens a file and then pass the content to the printWithDelay function to print its content"""
+    with open(path, 'r') as f:
+        printWithDelay(f.read())
 
-"""CLEANING"""
+if __name__ == "__main__":
 
-if os.path.exists('files/'):
-    os.rmdir('files')
+    command = 'wget -d -o files/{} -O files/{} {}'
+
+    """Getting some data to print"""
+    if not os.path.exists('files/'):
+        """making a new directory to put all the tmp files"""
+        os.mkdir('files')
+        os.system(command.format('output', 'data1', 'https://raw.githubusercontent.com/AntonioEstela/output-like-a-hacker.exe/main/logs/log-example-1.log'))
+        os.system(command.format('output_2','data2', 'https://raw.githubusercontent.com/AntonioEstela/monty/master/main.c'))    
+
+    """Open the files and then print its content"""
+    try:
+        openFile('files/output')
+        openFile('files/data1')
+        openFile('files/output_2')
+        openFile('files/data2')
+    except FileNotFoundError:
+        print("Wrong Path")
+
+    """CLEANING"""
+
+    if os.path.exists('files/'):
+        rmtree('files')
