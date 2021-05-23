1303e6f2fcbeebc839dd3c2d94f63616d9691eec
Antonio Estela <antonioestela73@gmail.com>
2021-05-23 16:07:19 -0500

    Final version (1.0)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from datetime import datetime
 import os
 from shutil import rmtree
 from sys import stdout
@@ -14,12 +15,17 @@ def printWithDelay(string):
         time.sleep(0.001)
     print()
 
+
 def openFile(path):
-    """Function that opens a file and then pass the content to the printWithDelay function to print its content"""
+    """Function that opens a file and then pass the content
+    to the printWithDelay function to print its content"""
     with open(path, 'r') as f:
         printWithDelay(f.read())
 
+
 if __name__ == "__main__":
+
+    startTime = time.time()
 
     command = 'wget -d -o files/{} -O files/{} {}'
 
@@ -28,7 +34,8 @@ if __name__ == "__main__":
         """making a new directory to put all the tmp files"""
         os.mkdir('files')
         os.system(command.format('output', 'data1', 'https://raw.githubusercontent.com/AntonioEstela/output-like-a-hacker.exe/main/logs/log-example-1.log'))
-        os.system(command.format('output_2','data2', 'https://raw.githubusercontent.com/AntonioEstela/monty/master/main.c'))    
+        os.system(command.format('output_2', 'data2', 'https://raw.githubusercontent.com/AntonioEstela/simple_shell/master/shell.c'))
+        os.system(command.format('output_3', 'data3', 'https://raw.githubusercontent.com/AntonioEstela/output-like-a-hacker.exe/main/logs/log-example-2.log'))
 
     """Open the files and then print its content"""
     try:
@@ -36,10 +43,20 @@ if __name__ == "__main__":
         openFile('files/data1')
         openFile('files/output_2')
         openFile('files/data2')
+        openFile('files/output_3')
+        openFile('files/data3')
     except FileNotFoundError:
         print("Wrong Path")
 
     """CLEANING"""
-
     if os.path.exists('files/'):
         rmtree('files')
+
+    """DONE"""
+    printWithDelay("[INFO]------------------------------------------------")
+    printWithDelay("[INFO] D O N E  [ ============================ ] 100%")
+    printWithDelay("[INFO]------------------------------------------------")
+    printWithDelay("[INFO] Total time: {}s".format(time.time() - startTime))
+    printWithDelay("[INFO] Finished at: {}".format(datetime.now()))
+    printWithDelay("[INFO] Total data: {}GB".format(os.path.getsize('main.py')))
+    printWithDelay("[INFO]------------------------------------------------")
